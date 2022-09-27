2a488e8df5277fd0cefa2a4c4b6a9edab602f85f
JIABAOHAN6 <113663994+JIABAOHAN6@users.noreply.github.com>
2022-09-27 22:31:46 +0900

    pull commit

--- src/main/java/com/gitclass/gittest.java
+++ src/main/java/com/gitclass/gittest.java
@@ -9,5 +9,6 @@ public class gittest {
         System.out.println("hello github!!!!!");
         System.out.println("hello github!!!!");
         System.out.println("push github!!!!");
+        System.out.println("pull github!!!!");
     }
 }
